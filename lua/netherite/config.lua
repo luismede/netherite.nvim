3614f51cf1258173a15276978b6bd6f82d2e257a
Luis Henrique <luishenrique23hh23@gmail.com>
2026-07-10 13:46:32 +0000

    doc: add type annotations for NetheriteConfig

--- lua/netherite/config.lua
+++ lua/netherite/config.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+---@class NetheriteConfig
+---@field vault_path string Path to the directory where notes are stored.
+---@field open_mode "float" | "split" Mode to open the note window. Can be "float" or "split".
+---@field default_filename string Default filename to use when creating a new note.
+
+---@type NetheriteConfig
 local default_config = {
 	vault_path = vim.fn.stdpath("data"),
 	open_mode = "float",
@@ -8,10 +14,21 @@ local default_config = {
 
 M.config = default_config
 
-M.setup = function(opts)
+---@param opts NetheriteConfig
+function M.setup(opts)
 	opts = opts or {}
 
 	M.config = vim.tbl_deep_extend("force", default_config, opts)
+end
+
+---@param open_mode "float" | "split"
+function M.mode(open_mode)
+	local valid = {split = true, float = true}
+	if not valid[open_mode] then
+		vim.notify("Invalid mode. Valid modes are: split, float", vim.log.levels.ERROR)
+		return
+	end
+	M.config.open_mode = open_mode
 end
 
 return M
